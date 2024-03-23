9a652d2f3bbd2088df7bed0d00446af539fa118e
Dyuzhick <161967842+dyuzhick@users.noreply.github.com>
2024-03-23 17:21:04 +0300

    Delete shrift.bat

--- fonts/shrift.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-start https://clck.ru/39bcmq
-
-timeout /t 8 /nobreak >nul
-mkdir "You have been rickrolled"
-echo You have been rickrolled! Have a nice day! > "You have been rickrolled\You have been rickrolled.txt"
-start "You have been rickrolled" "You have been rickrolled\You have been rickrolled.txt"
